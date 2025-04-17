2b10e3a2f1438424b2cd26090b181e6595611055
lucky233gays <15802052351@163.com>
2025-04-17 17:48:34 +0800

    Add files via upload

---
+++ src/settings.2aa1b.js
@@ -0,0 +1,21 @@
+window._CCSettings = {
+    platform: "web-mobile",
+    groupList: ["default", "fruit", "wall", "downwall"],
+    collisionMatrix: [
+        [true],
+        [false, true, true, true],
+        [false, true, false],
+        [false, true, false, false]
+    ],
+    hasResourcesBundle: false,
+    hasStartSceneBundle: false,
+    remoteBundles: [],
+    subpackages: [],
+    launchScene: "db://assets/Scene/MainGameSence.fire",
+    orientation: "",
+    jsList: [],
+    bundleVers: {
+        internal: "279e0",
+        main: "10d96"
+    }
+};
